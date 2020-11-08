e8720d9f6f445d8edd24af3bbed16f3367961023
valmyr <65004196+valmyr@users.noreply.github.com>
2020-11-07 22:37:53 -0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -4,13 +4,8 @@ using namespace std;
 #include"./Conta.cpp"
 #include"./ContaPoupanca.h"
 int main(){
-    Endereco p;
-    p.CEP ="13737838374";
-    p.cidade ="Alagoas";
-    p.estado = "Arapiraca";
-    p.rua = "rt";
+    Endereco p = {"13737838374","Alagoas","Arapiraca","rt"};
     Conta *b = new ContaPoupanca("Valmir Ferreira da Silva",1289373876,21,300000,p);
-
     b->imprimir();
     b->sacar(1000);
     b->imprimir();
