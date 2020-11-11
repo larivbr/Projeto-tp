60695f29ead914fca864981fc3131b4bebf00ead
cordeiroewerton <cordeiroewerton13@gmail.com>
2020-11-11 10:54:03 -0300

    usuario att

--- Header Files/Usuario.h
+++ Header Files/Usuario.h
@@ -8,24 +8,21 @@ struct Endereco{
 };
 class Usuario{
   private:
-      string nomeDoUsuario;
-      int idade;
-      long int CPF;
-      Endereco enderecoDoUsuario; // tu implementa essa aqui no construtor, valmyr.
-      //Eu não sei como tu fez para implementar no teu codigo
-      //precisamos instânciar o objeto tbm, lembrando que o mesmo usuario pode ter uma conta corrente e uma poupança
+        string nomeDoUsuario;
+        int idade;
+        long int CPF;
+        Endereco enderecoDoUsuario;
   public:
 
-      Usuario(const string & = "0",const int & = 0,const long int & = 0,/*Recurso c++ 11 permite iniciar structs/classes dessa forma*/const Endereco& = {"0","0","0","0"});
-      void setIdade(const int &);
-      void setCPF(const long long &);
-      void setEnderecoDoUsuario(const Endereco &);
-      void setNomeDoUsuario(const string &);
-
-      string getNomeDoUsuario()const;
-      long int getCPF()const;
-      int getIdade()const;
-      string getEnderecoDoUsuario()const;
+        Usuario(const string & = "0",const int & = 0,const long int & = 0,/*Recurso c++ 11 permite iniciar structs/classes dessa forma*/const Endereco& = {"0","0","0","0"});
+        void setIdade(const int &);
+        void setCPF(const long int &);
+        void setEnderecoDoUsuario(const Endereco &);
+        void setNomeDoUsuario(const string &);
+        string getNomeDoUsuario()const;
+        long int getCPF()const;
+        int getIdade()const;
+        string getEnderecoDoUsuario()const;
 
 };
 
--- Resource Files/Usuario.cpp
+++ Resource Files/Usuario.cpp
@@ -1,9 +1,41 @@
 #include "..\Header Files\Usuario.h"
 Usuario::Usuario(const string & nomeDoUsuario,const int & idade,const long int & CPF,/*Recurso c++ 11 permite iniciar structs/classes dessa forma*/const Endereco & enderecoDoUsuario){
-	this->nomeDoUsuario = nomeDoUsuario;
-	this->idade = idade;
-	this->CPF =CPF;
-	this->enderecoDoUsuario;
+	this->nomeDoUsuario=nomeDoUsuario;
+	this->idade=idade;
+	this->CPF=CPF;
+	this->enderecoDoUsuario=enderecoDoUsuario;
+}
+
+void Usuario::setIdade(const int & idade){
+	this->idade=idade;
+}
+
+int Usuario::getIdade() const{
+	return idade;
+}
+
+void Usuario::setCPF(long int & CPF){
+	this->CPF=CPF;
+}
+
+long int Usuario:;getCPF() const{
+	return CPF;
+}
+
+void Usuario::setEnderecoDoUsuario(const Endereco & end){
+	enderecoDoUsuario=end;
+}
+
+Endereco Usuario::getEnderecoDoUsuario() const{
+	return enderecoDoUsuario;
+}
+
+void Usuario::setNomeDoUsuario(const string & nomeDoUsuario){
+	this->nomeDoUsuario=nomeDoUsuario;
+}
+
+string Usuario::getNomeDoUsuario() const{
+	return nomeDoUsuario;
 }
 
 
